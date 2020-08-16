5b160984ab4c4e2efc33a7b269ceb70717110464
Chris-Howells <69651150+Chris-Howells@users.noreply.github.com>
2020-08-16 11:54:44 +0100

    New Python notebook added

---
+++ Jupyter Tutorial-Copy1.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World")
+
+
+# In[3]:
+
+
+i = 1
+while i <= 10
+print(i)
+i = i + 1
+
+
+# In[4]:
+
+
+i = 1
+while i <= 10
+print(i)
+i = i + 1
+
+
+# In[10]:
+
+
+i = 1
+while i <= 10
+print(i)
+i = i + 1
+
+
+# In[12]:
+
+
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
+
+
+# In[15]:
+
+
+#This is a headline
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
